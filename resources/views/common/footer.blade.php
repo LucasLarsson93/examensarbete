cc41dc4cf7c003586ff8012f39bddfcbc1ef396e
KungLucas <lucas.larsson93@hotmail.com>
2024-05-21 14:15:32 +0200

    feat: including the api data into the footer.

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,9 +1,16 @@
 {{-- Footer --}}
 <footer class="bg-white border-t border-gray-200">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <p class="text-sm text-gray-600">Forum built with Laravel by <strong>Lucas Larsson</strong>.</p>
+        <p class="text-sm text-gray-600">Forum built with Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }}) |  by <strong>Lucas Larsson</strong>.</p>
     </div>
-
+    {{-- Forum total register users and newest member --}}
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="text-sm text-gray-600">
+            Total members: <span id="total-users" style="margin-right:2rem;"></span>
+            {{-- Display latest registered user. --}}
+            Newest member: <span id="latest-user"></span>
+        </div>
+    </div>
 </footer>
 <script>
 // Select all elements with IDs 'post_content' and 'content'
